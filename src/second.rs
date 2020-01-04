b888c18d9b4d1bf75f99f29fa28ad36f5a95d078
Muhammad Falak R Wani <falakreyaz@gmail.com>
2020-01-04 10:24:37 +0530

    second: impl IterMut for List
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- src/second.rs
+++ src/second.rs
@@ -87,6 +87,27 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {next: self.head.as_mut().map(|n| &mut **n) }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|n| {
+            self.next = n.next.as_mut().map(|n| & mut **n);
+            & mut n.elem
+        })
+    }
+}
+
 
 mod test {
     use super::List;
@@ -152,5 +173,16 @@ mod test {
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 }
