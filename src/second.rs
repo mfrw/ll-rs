e9f2116d54622bad84aa583f443ff823b8c91178
Muhammad Falak R Wani <falakreyaz@gmail.com>
2020-01-04 10:13:32 +0530

    second: impl iter for List
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- src/second.rs
+++ src/second.rs
@@ -67,6 +67,27 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
+pub struct Iter<'a,T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter{next: self.head.as_ref().map(|n| &**n) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+      fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
+    }
+}
+
+
 mod test {
     use super::List;
     #[test]
@@ -120,5 +141,16 @@ mod test {
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
